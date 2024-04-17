2fe5c9c6f81b1ffc26e336b64fc407a6e92bed80
shubham Rawat <rawatshubam269@gmail.com>
2024-04-17 21:58:30 +0530

    Firebase setup done

---
+++ src/firebase/firebase.js
@@ -0,0 +1,22 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAuth, GoogleAuthProvider } from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
+import { getStorage } from "firebase/storage";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyC3a5wUkBIjxKfU0jyi7G0TPTgFONZNRkc",
+  authDomain: "communer-c7b6b.firebaseapp.com",
+  projectId: "communer-c7b6b",
+  storageBucket: "communer-c7b6b.appspot.com",
+  messagingSenderId: "1090616358269",
+  appId: "1:1090616358269:web:c377ec05b3bbc585d1c814",
+  measurementId: "G-8QRZDJMTWJ",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+export const auth = getAuth();
+export const provider = new GoogleAuthProvider();
+export const storage = getStorage();
+export const db = getFirestore(app);
